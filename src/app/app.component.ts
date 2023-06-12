b3f247019c1a729f4d852a86f4036f24b6cd2ac0
Cam努 <98275835+cambrv@users.noreply.github.com>
2023-06-12 12:09:02 -0500

    Trabajo de clase exploración

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+//Comunica el html y css 
+//Comunica los end points. Toda esa programación
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
